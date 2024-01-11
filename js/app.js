1e6a38bb6540e5f707a81a4d5b46577d62c5bc92
Sarkor <abilkosimovsarkor@gmail.com>
2024-01-11 21:25:31 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let burgerBtn = document.querySelector('.navbar__nav--burger')
+let modal = document.querySelector('.modal')
+let burgerBtnLine = document.querySelector('.navbar__nav--burger')
+
+burgerBtn.addEventListener('click', () => {
+    modal.classList.toggle('active')
+    burgerBtnLine.classList.toggle('active')
+})
+
+let actionBtn = document.querySelector('.btn-send-action')
+let actionBlock = document.querySelector('.action')
+
+actionBtn.addEventListener('click', () => {
+    actionBlock.classList.add('active')
+})

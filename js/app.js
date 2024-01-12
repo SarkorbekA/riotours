54fb6c3c7d90eb98c0ce0f9a31cbb234d3ce0c91
Sarkor <abilkosimovsarkor@gmail.com>
2024-01-12 20:18:34 +0500

    feat: optimize project

--- js/app.js
+++ js/app.js
@@ -1,19 +1,46 @@
-let burgerBtn = document.querySelector('.navbar__nav--burger')
-let modal = document.querySelector('.modal')
-let burgerBtnLine = document.querySelector('.navbar__nav--burger')
+let burgerBtn = document.querySelector('.navbar__nav--burger');
+let modal = document.querySelector('.modal');
+let burgerBtnLine = document.querySelector('.navbar__nav--burger');
 
 burgerBtn.addEventListener('click', () => {
-    modal.classList.toggle('active')
-    burgerBtnLine.classList.toggle('active')
+    modal.classList.toggle('active');
+    burgerBtnLine.classList.toggle('active');
 })
 
-let actionBtn = document.querySelector('.btn-send-action')
-let actionBlock = document.querySelector('.action')
+let actionBtn = document.querySelector('.btn-send-action');
+let actionBlock = document.querySelector('.action');
 
-actionBtn.addEventListener('click', () => {
-    actionBlock.classList.add('active')
-})
+// actionBtn.addEventListener('click', () => {
+//     actionBlock.classList.add('active')
+// })
 
+const sliderDropdownBtn = document.querySelectorAll('.slider__buttons-item.second');
+const sliderDropdown = document.querySelectorAll('.slider__dropdown');
+
+function checkActive(item) {
+    if (item.classList.contains('active')) {
+        item.style.minHeight = item.scrollHeight + 'px';
+    } else {
+        item.style.minHeight = '0';
+    }
+}
+
+for (let i = 0; i < sliderDropdownBtn.length; i++) {
+    sliderDropdownBtn[i].addEventListener('click', () => {
+        sliderDropdown[i].classList.toggle('active');
+        sliderDropdownBtn[i].classList.toggle('active')
+        checkActive(sliderDropdown[i]);
+    })
+}
+
+
+function removeActive() {
+    for (let j = 0; j < sliderDropdownBtn.length; j++) {
+        sliderDropdownBtn[j].classList.remove('active');
+        sliderDropdown[j].classList.remove('active')
+        checkActive(sliderDropdown[j]);
+    }
+};
 
 var swiper = new Swiper(".mySwiper", {
     loop: true,
@@ -21,4 +48,12 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    on: {
+        slideChange: function () {
+            removeActive();
+        }
+    }
 });
+
+
+
